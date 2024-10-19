33468e4becd9c36d21fc9c7a98d1381aba0607d4
Ali Al-Ebrahim <84152592+mi3law@users.noreply.github.com>
2024-10-19 02:12:08 +0200

    Create 0_basic_clam__with_pain.py
    Experimentation and testing of pain control neurons in Arch class and compatibility with core, for the Pavlov app by @Rafipilot
    
    Note: I've not yet tested that this works technically (the way to test it would be to complete a simple pain function and then test it in the clam, to see that the signals are passing through properly from Arch to Agent). Testing that this works functionally is application dependent, the next step (what you'll do in the Pavlov app).

---
+++ Architectures/0_basic_clam__with_pain.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+// aolabs.ai software >ao_core/Arch.py (C) 2023 Animo Omnis Corporation. All Rights Reserved.
+
+Thank you for your curiosity!
+"""
+import numpy as np
+
+
+## // Basic Clam -- Reference Design #0
+# 
+# Our simplest Agent, our 'hello, world.'
+#
+# For interactive visual representation of this Arch:
+#    https://miro.com/app/board/uXjVM_kESvI=/?share_link_id=72701488535
+#
+# Customize and upload this Arch to our API to create Agents: https://docs.aolabs.ai/reference/kennelcreate
+#
+
+
+description = "Basic Clam"
+arch_i = [1, 1, 1]     # 3 neurons, 1 in each of 3 channels, corresponding to Food, Chemical-A, Chemical-B (present=1/not=0)
+arch_z = [1]           # corresponding to Open=1/Close=0
+arch_c = [2]           # adding 2 control neuron which we'll define with the instinct control function below
+connector_function = "full_conn"
+
+# To maintain compatibility with our API, do not change the variable name "Arch" or the constructor class "ar.Arch" in the line below
+Arch = ar.Arch(arch_i, arch_z, arch_c, connector_function, description)
+
+
+# Adding Instinct Control Neuron - PLEASURE (Positive Reinforcement)
+c0 = 3  # set-relative id of neuron; the first 3 neurons (ids 0, 1, 2) are included in all Archs by default (corresponding to LABEL, Cpos, Cneg)
+def c0_instinct_rule(INPUT, Agent):
+    if INPUT[0] == 1    and    Agent.story[ Agent.state-1,  Agent.arch.Z__flat[0]] == 1 :        # self.Z__flat[0] needs to be adjusted as per the agent, which output the designer wants the agent to repeat while learning postively or negatively
+        instinct_response = [1, "c0 instinct triggered"]
+    else:
+        instinct_response = [0, "c0 pass"]    
+    return instinct_response            
+# Saving the function to the Arch so the Agent can access it
+Arch.datamatrix[4, Arch.C[1][0]] = c0_instinct_rule
+# Labelling the neuron as a Pleasure neuron for ao_core digestion
+Arch.C__flat_pleasure = np.append(Arch.C__flat_pleasure, Arch.C__flat[c0])
+
+
+# Adding Instinct Control Neuron - PAIN (Negative Reinforcement)
+c1 = 4 # set-relative id of neuron
+def c1_instinct_rule(INPUT, Agent):
+    # 
+    # Define instinct logic here
+    #       
+    return
+# Saving the function to the Arch so the Agent can access it
+Arch.datamatrix[4, Arch.C[1][1]] = c1_instinct_rule
+# Labelling the neuron as a Pain neuron for ao_core digestion
+Arch.C__flat_pain = np.append(Arch.C__flat_pain, Arch.C__flat[c1])
